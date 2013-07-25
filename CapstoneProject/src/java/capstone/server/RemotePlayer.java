eef5aacf5559eabf114c5e41451028a0e960bc64
Max <Max@192.168.1.7>
2013-07-25 20:56:00 +1200

    Added constructor for RemotePlayer.

--- CapstoneProject/src/java/capstone/server/RemotePlayer.java
+++ CapstoneProject/src/java/capstone/server/RemotePlayer.java
@@ -18,6 +18,10 @@ public class RemotePlayer implements Player {
     
     private HttpSession session;
 
+    public RemotePlayer(HttpSession session) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
     public HttpSession getSession() {
         return session;
     }
