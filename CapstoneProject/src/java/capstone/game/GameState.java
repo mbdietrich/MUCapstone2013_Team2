97ab2857a0aa319738297e91d8b9862b7aa16751
Sarsha Jones <stameidyx@hotmail.com>
2013-07-25 17:18:52 +1200

    SuperGame and GameState merged
    I've basically just copied the methods and properties from SuperGame to
    GameState.
    I'm just about to start changing the methods itself, and I thought I
    should commit this separately so we can just roll it back if something
    blows up in my face

--- CapstoneProject/src/java/capstone/game/GameState.java
+++ CapstoneProject/src/java/capstone/game/GameState.java
@@ -10,16 +10,25 @@ package capstone.game;
 public class GameState {
 	
 	private int CurrentPlayer; // 1 = Player 1 (X), 2 = Player 2 (O)
-	protected SuperGame CurrentGame;
 	protected SubGame SelectedSubgame;
+	private SubGame[][] mainboard;
+	private int[][] statusboard; // contains the STATUSES ONLY of the subgames in mainboard
 	
 	/**
-	 * Constructor. Initializes the variables and the games
+	 * Constructor. 
+	 * Initializes the variables and creates the empty subgames.
 	 */
     public GameState() {
-    	CurrentGame = new SuperGame();
     	//TODO: Better selection for the starting player? i.e. Random, or winner of last game
     	CurrentPlayer = 1;
+		mainboard = new SubGame[3][3];
+		statusboard = new int[3][3];
+		for (int i=0; i<3; i++){
+			for (int j =0; j<3; j++){
+				mainboard[i][j]= new SubGame();
+				statusboard[i][j] = 0;
+			}
+		}
     }
     
     public void PlacePiece(int superGameX, int superGameY, int subGameX, int subGameY){
@@ -28,12 +37,12 @@ public class GameState {
     	 */
     	
     	// Get subgame from the right section.
-    	SelectedSubgame = CurrentGame.GetSubGame(superGameX, superGameY);
+    	SelectedSubgame = mainboard[superGameX][superGameY];
     	//TODO: Throw an exception if the subgame is closed? at the moment, the move is simply ignored.
     	if (SelectedSubgame.getStatus() == 0){
 			//Set the player's piece (validity will be checked in the subgame class)
 			SelectedSubgame.setGamePiece(subGameX, subGameY, CurrentPlayer);
-			CurrentGame.setStatusboard(superGameX, superGameY);
+			setStatusboard(superGameX, superGameY);
 			nextTurn();
     	}
     }
@@ -45,6 +54,85 @@ public class GameState {
     	if (CurrentPlayer == 1) CurrentPlayer = 2;
     	if (CurrentPlayer == 2) CurrentPlayer = 1;
     }
+    
+	/**
+	 * This returns the subgame in the specified location.
+	 * NOTE: This returns a variable of the SUBGAME class. To get the board as a 2D array only, use GetSubBoard().
+	 * @param x Column (0 - 2)
+	 * @param y Row (0 - 2)
+	 * @return The SubGame variable stored in the given location of the main game
+	 */
+	public SubGame GetSubGame(int x, int y){
+		return mainboard[x][y];
+	}
+	
+	/**
+	 * This returns the subgame in the specified location.
+	 * NOTE: This returns the subgame as a 2D array. To get the board as a SUBGAME variable (which includes the 
+	 * game's status), use GetSubGame().
+	 * @param x Column of the top-level board (0 - 2)
+	 * @param y Row of the top-level board (0 - 2)
+	 * @return The board stored in the SubGame of the given location.
+	 */
+	public int[][] GetSubBoard(int x, int y){
+		return mainboard[x][y].getBoard();
+	}
+	
+	/**
+	 * This replaces the subgame in the given location with the subgame passed into the method.
+	 * NOTE: This requires you to have a SUBGAME variable, complete with the correct(!!) "status" property. 
+	 * If you only have an integer array, use SetSubBoard().
+	 * @param x Column of the top-level board (0 - 2)
+	 * @param y Row of the top-level board (0 - 2)
+	 * @param newsubgame The new subgame to replace the current one in the given location
+	 */
+	public void SetSubGame(int x, int y, SubGame newsubgame){
+		//TODO: check validity of the board
+		mainboard[x][y] = newsubgame;
+	}
+	
+	/**
+	 * This replaces the subgame in the given location with the game passed into the method.
+	 * NOTE: This only requires the board as a 3 x 3 array of integers (assuming the correct representation
+	 * of the board with 0s, 1s and 2s). The status of the board is not needed. If you already have a SUBGAME 
+	 * variable, use SetSubGame().
+	 * @param x Column of the top-level board (0 - 2)
+	 * @param y Row of the top-level board (0 - 2)
+	 * @param newsubboard The new 3 x 3 integer array to replace the board in the given location
+	 */
+	public void SetSubBoard(int x, int y, int[][] newsubboard){
+		//TODO: check validity of the board
+		mainboard[x][y].setBoard(newsubboard);
+		mainboard[x][y].setStatus(GameRules.findWinner(newsubboard));
+	}
+	
+	public int[][] getStatusboard() {
+		return statusboard;
+	} 
+	
+	/**
+	 * Update the statusboard for a given subgame. If there has been a change, check to see if there is an overall winner.
+	 * @param x Column of the top-level board (0 - 2)
+	 * @param y Row of the top-level board (0 - 2)
+	 */
+	public void setStatusboard(int x, int y){
+		if (statusboard[x][y] != mainboard[x][y].getStatus()){
+			// A subgame has been completed.  update the statusboard
+			statusboard[x][y] = mainboard[x][y].getStatus();
+			// Is there a winner yet?
+			if (GameRules.findWinner(statusboard)!= 0){
+				//TODO: Game is complete! What now?
+			}
+		}
+	}
+
+	public SubGame[][] getMainboard() {
+		return mainboard;
+	}
+
+	public void setMainboard(SubGame[][] mainboard) {
+		this.mainboard = mainboard;
+	}
 
 	public int getCurrentPlayer() {
 		return CurrentPlayer;
@@ -54,13 +142,6 @@ public class GameState {
 		CurrentPlayer = currentPlayer;
 	}
 
-	public SuperGame getCurrentGame() {
-		return CurrentGame;
-	}
-
-	public void setCurrentGame(SuperGame currentGame) {
-		CurrentGame = currentGame;
-	}
 
 	public SubGame getSelectedSubgame() {
 		return SelectedSubgame;
