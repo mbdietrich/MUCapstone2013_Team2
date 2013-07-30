ba6a3aff23840b4636c468c78a7db73ac9a57278
Max <Max@192.168.1.7>
2013-07-30 13:43:51 +1200

    Added GameCreate Servlet, registered to web.xml

---
+++ CapstoneProject/src/java/capstone/server/GameCreate.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone.server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Max
+ * 
+ * Create a new game for a player to join
+ */
+public class GameCreate extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        GameManager.newGame(request.getSession());
+        this.getServletContext().getRequestDispatcher("/game.jsp").forward(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Create a new game";
+    }
+}
