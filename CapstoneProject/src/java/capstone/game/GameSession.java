d8e18c3a18cd9e39744f4c03c1a077cb7f73f146
Sarsha Jones <stameidyx@hotmail.com>
2013-07-30 13:50:09 +1200

    Play() added to GameSession, and partially implemented. Not having the player number as an integer is getting somewhat annoying, maybe Player should have this as a property? Or would it be simpler to make a method in GameSession that returns it as an integer?

--- CapstoneProject/src/java/capstone/game/GameSession.java
+++ CapstoneProject/src/java/capstone/game/GameSession.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public class GameSession {
 
 	private GameState currentgame;
-	private Player player1, player2;
+	private Player player1, player2, currentPlayer, gameWinner;
 
         public final String SessionID = UUID.randomUUID().toString();
         
@@ -16,7 +16,7 @@ public class GameSession {
 	 */
 	public GameSession() {
 		/*
-		 * When both players are ready, create the GameState. While the game is
+		 * TODO: When both players are ready, create the GameState. While the game is
 		 * not finished, prompt the player for a move When the game is finished,
 		 * display a message
 		 */
@@ -30,6 +30,35 @@ public class GameSession {
                     player2=player;
                 }
                 else throw new IllegalGameException("Player "+player.toString()+" tried to join a full game.");
+	}
+	
+	public void Play(){
+		//Get the current player's move
+		Coordinates newMove = new Coordinates(0,0,0,0);
+		switch(currentgame.getCurrentPlayer()){
+		case 1: newMove = player1.next(currentgame, 1);
+				currentPlayer = player1;
+				break;
+		case 2: newMove = player2.next(currentgame, 2);
+				currentPlayer = player2;
+				break;
+		}
+		
+		if (GameRules.validMove(currentgame, newMove)){
+			// if this move is valid
+			currentgame.PlacePiece(newMove);
+		}
+		else {
+			//if this move is not valid, the player is forced out for now
+			Leave(currentPlayer);
+		}
+		if (!GameRules.isDone(currentgame)){
+			//if the game is not finished, call the function again
+			Play();
+		}
+		else{
+			//TODO: if the game is finished, update the properties
+		}
 	}
 
 	public void Leave(Player player) {
