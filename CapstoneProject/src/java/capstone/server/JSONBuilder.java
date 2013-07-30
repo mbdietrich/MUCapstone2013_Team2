bb218409b86d7adcf8bf9ec93baae1365f115c87
Max <Max@192.168.1.7>
2013-07-30 14:16:55 +1200

    Added JSONBuilder, a utility class to build JSON for fetching game state

---
+++ CapstoneProject/src/java/capstone/server/JSONBuilder.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone.server;
+
+import capstone.game.GameSession;
+import capstone.player.Player;
+
+/**
+ *
+ * @author Max
+ * Utility class to convert the necessary information from GameSession into a JSON object
+ */
+public class JSONBuilder {
+    
+    public static String buildJSON(GameSession game, Player target){
+        StringBuilder builder = new StringBuilder();
+        
+        //TODO build JSON object
+        
+        return builder.toString();
+    }
+    
+}
