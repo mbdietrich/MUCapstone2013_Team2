7adfe9425be9fa2dd99d4ef93619373976d99254
Sarsha Jones <stameidyx@hotmail.com>
2013-07-30 16:48:56 +1200

    JSON builder implemented. It hasn't been tested though, some parts may be wrong.

--- CapstoneProject/src/java/capstone/server/JSONBuilder.java
+++ CapstoneProject/src/java/capstone/server/JSONBuilder.java
@@ -14,12 +14,106 @@ import capstone.player.Player;
  */
 public class JSONBuilder {
     
-    public static String buildJSON(GameSession game, Player target){
+    public static String buildJSON(GameSession game, RemotePlayer target){
         StringBuilder builder = new StringBuilder();
         
-        //TODO build JSON object
+        builder = builder
+        .append(buildPlayerNumber(game, target))
+        .append(buildIsTurn(game,target))
+        .append(buildGameStatus(game, target))
+        .append(buildBoardObject(game));
         
         return builder.toString();
     }
     
+    public static String buildPlayerNumber(GameSession game, RemotePlayer target){
+    	StringBuilder builder = new StringBuilder();
+    	
+    	int playerNumber = game.getPlayerNumber(target);
+    	
+		builder = builder
+		.append('{')
+		.append("\"PlayerNumber\":\"").append(Integer.toString(playerNumber)).append("\"")
+		.append('}');
+		
+		return builder.toString();
+    }
+    
+    public static String buildIsTurn(GameSession game, RemotePlayer target){
+    	StringBuilder builder = new StringBuilder();
+    	
+    	String turn = Boolean.toString(target.isActive());
+    	
+		builder = builder
+		.append('{')
+		.append("\"isTurn\":\"").append(turn).append("\"")
+		.append('}');
+		
+		return builder.toString();
+    }
+    
+    public static String buildGameStatus(GameSession game, RemotePlayer target){
+    	StringBuilder builder = new StringBuilder();
+    	
+    	String winner = Integer.toString(game.getCurrentGame().getWinner());
+    	
+		builder = builder
+		.append('{')
+		.append("\"Status\":\"").append(winner).append("\"")
+		.append('}');
+		
+		return builder.toString();
+    }
+    
+    public static String buildBoardObject(GameSession game){
+    	StringBuilder builder = new StringBuilder();
+    	
+    	builder = builder
+		.append('{')
+		.append("\"Board\":").append(buildBoardOnly(game))
+		.append('}');
+    	
+    	return builder.toString();
+    }
+    
+    public static String buildBoardOnly(GameSession game){
+    	StringBuilder builder = new StringBuilder();
+    	
+    	int[][] board1,board2,board3;
+    	
+    	builder = builder.append('{');
+    	for (int i = 0; i < 3; i++){
+    		// get each row of subgames
+    		board1 = game.getCurrentGame().GetSubBoard(0, i);
+    		board2 = game.getCurrentGame().GetSubBoard(1, i);
+    		board3 = game.getCurrentGame().GetSubBoard(2, i);
+    		// get each row of integers in the row of subgames
+    		builder = builder.append('{');
+    		for (int j = 0; j < 3; j++){
+    			builder = builder.append(buildRow(board1,j)).append(',')
+    			.append(buildRow(board2,j)).append(',')
+    			.append(buildRow(board3,j));
+    		}
+    		builder = builder.append('}');
+    		if (i<2){
+    			builder = builder.append(',');
+    		}
+    	}
+    	builder.append('}');
+    	
+    	return builder.toString();
+    }
+    
+    public static String buildRow(int[][] board, int rownumber){
+    	StringBuilder builder = new StringBuilder();
+    	
+    	for (int i = 0; i < 3; i++){
+    		builder = builder.append("\"Integer.toString(board[i][rownumber])\"");
+    		if (i<2){
+    			builder = builder.append(',');
+    		}
+    	}
+    	
+    	return builder.toString();
+    }
 }
