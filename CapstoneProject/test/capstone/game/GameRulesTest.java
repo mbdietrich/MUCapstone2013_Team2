3e0d0aa208a86e987b96663da661c36b63a5de4a
Ameides <stameidyx@hotmail.com>
2013-08-16 11:46:00 +1200

    testCheckStatusBoard now tests draws correctly.

--- CapstoneProject/test/capstone/game/GameRulesTest.java
+++ CapstoneProject/test/capstone/game/GameRulesTest.java
@@ -93,11 +93,11 @@ public class GameRulesTest {
 
         board = new int[3][3];
         board[0][0] = 1;
-        board[0][1] = 2;
-        board[0][2] = 1;
+        board[0][1] = 1;
+        board[0][2] = 2;
         board[1][0] = 2;
-        board[1][1] = 1;
-        board[1][2] = 2;
+        board[1][1] = 2;
+        board[1][2] = 1;
         board[2][0] = 1;
         board[2][1] = 2;
         board[2][2] = 1;
