34e699da14cb8521c4af403141ef66fa5b2a0568
Ameides <stameidyx@hotmail.com>
2013-08-16 12:17:27 +1200

    Test for winners has been broken up from one large test to a series of smaller tests.

--- CapstoneProject/test/capstone/game/GameRulesTest.java
+++ CapstoneProject/test/capstone/game/GameRulesTest.java
@@ -29,69 +29,123 @@ public class GameRulesTest {
     }
 
     /**
-     * Test of checkStatusBoard method, of class GameRules.
+     * Test a win across the first row
      */
     @Test
-    public void testCheckStatusBoard() {
+    public void testRow1(){
         int[][] board = new int[3][3];
-        ArrayList result = new ArrayList();
-        ArrayList expResult = new ArrayList();
         board[0][0] = 1;
         board[1][0] = 1;
         board[2][0] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(1);
 
-        board = new int[3][3];
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 1);
+    }
+
+     /**
+     * Test a win across the second row
+     */
+    @Test
+    public void testRow2(){
+        int[][] board = new int[3][3];
         board[0][1] = 1;
         board[1][1] = 1;
         board[2][1] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(1);
 
-        board = new int[3][3];
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 1);
+    }
+
+    /**
+     * Test a win across the third row
+     */
+    @Test
+    public void testRow3(){
+        int[][] board = new int[3][3];
         board[0][2] = 1;
         board[1][2] = 1;
         board[2][2] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(1);
 
-        board = new int[3][3];
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 1);
+    }
+
+     /**
+     * Test a win across the first column
+     */
+    @Test
+    public void testCol1(){
+        int[][] board = new int[3][3];
         board[0][0] = 1;
         board[0][1] = 1;
         board[0][2] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(1);
 
-        board = new int[3][3];
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 1);
+    }
+
+    /**
+     * Test a win across the second column
+     */
+    @Test
+    public void testCol2(){
+        int[][] board = new int[3][3];
         board[1][0] = 1;
         board[1][1] = 1;
         board[1][2] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(1);
 
-        board = new int[3][3];
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 1);
+    }
+
+    /**
+     * Test a win across the third column
+     */
+    @Test
+    public void testCol3(){
+        int[][] board = new int[3][3];
         board[2][0] = 1;
         board[2][1] = 1;
         board[2][2] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(1);
 
-        board = new int[3][3];
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 1);
+    }
+
+    /**
+     * Test a win across the diagonal from top left to bottom right
+     */
+    @Test
+    public void testDiagonal1(){
+        int[][] board = new int[3][3];
         board[0][0] = 1;
         board[1][1] = 1;
         board[2][2] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(1);
 
-        board = new int[3][3];
-        board[0][2] = 1;
-        board[1][1] = 1;
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 1);
+    }
+
+     /**
+     * Test a win across the diagonal from top right to bottom left
+     */
+    @Test
+    public void testDialongal2(){
+        int[][] board = new int[3][3];
         board[2][0] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(1);
+        board[1][1] = 1;
+        board[0][2] = 1;
 
-        board = new int[3][3];
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 1);
+    }
+
+     /**
+     * Test a a game which has been drawn (closed game, no winner)
+     */
+    @Test
+    public void testDraw(){
+        int[][] board = new int[3][3];
         board[0][0] = 1;
         board[0][1] = 1;
         board[0][2] = 2;
@@ -101,10 +155,9 @@ public class GameRulesTest {
         board[2][0] = 1;
         board[2][1] = 2;
         board[2][2] = 1;
-        result.add(GameRules.checkStatusBoard(board));
-        expResult.add(3);
 
-        assertEquals(expResult, result);
+        int result = (GameRules.checkStatusBoard(board));
+        assertEquals(result, 3);
     }
 
     /**
