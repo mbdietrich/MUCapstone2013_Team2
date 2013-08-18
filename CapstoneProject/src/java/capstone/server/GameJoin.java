fba66ed412cf83d072d438416fc3b35f9baa5231
Max <Max@192.168.1.7>
2013-08-19 11:57:40 +1200

    Created GameJoin.java, registered it to Requests.txt and web.xml

---
+++ CapstoneProject/src/java/capstone/server/GameJoin.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstone.server;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Max
+ */
+public class GameJoin extends HttpServlet {
+    
+    //Get all open game session IDs
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+    
+    //Join an open game
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+    }
+}
