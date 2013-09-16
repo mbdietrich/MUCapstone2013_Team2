1b5f14a7a6d225de506b696aa18381efba1b5ca6
Max <Max@192.168.1.7>
2013-09-16 13:49:23 +1200

    Added CapstoneServerTests project.

---
+++ CapstoneServerTests/src/capstoneservertests/CapstoneServerTests.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capstoneservertests;
+
+/**
+ *
+ * @author Max
+ */
+public class CapstoneServerTests {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
