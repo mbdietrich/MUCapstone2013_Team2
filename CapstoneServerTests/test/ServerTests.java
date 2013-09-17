23242f2af875e8ed5d13f37678653d8c8a10d35b
Max <Max@192.168.1.7>
2013-09-18 11:53:01 +1200

    Test now run off localhost.

--- CapstoneServerTests/test/ServerTests.java
+++ CapstoneServerTests/test/ServerTests.java
@@ -5,13 +5,14 @@
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.junit.After;
@@ -27,7 +28,7 @@ import static org.junit.Assert.*;
  */
 public class ServerTests {
     
-    private static final String URL = "http://capstoneg2.jelastic.servint.net/CapstoneProject/";
+    private static final String URL = "http://localhost:8080/CapstoneProject/";
     private static final String userName = "ApacheTest";
     private static final String password = "testPass";
     
@@ -48,19 +49,31 @@ public class ServerTests {
     }
     
     @Test
-    public void testLogin() throws UnsupportedEncodingException, IOException{
+    public void testConnect() throws UnsupportedEncodingException, IOException{
         HttpClient client = new DefaultHttpClient();
-        HttpPost post = new HttpPost(URL+"login");
+        URI uri = URI.create(URL+"index.jsp");
+        System.out.println(uri);
+        HttpGet get = new HttpGet(uri);
         
-        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-        nameValuePairs.add(new BasicNameValuePair("userName", userName));
-        nameValuePairs.add(new BasicNameValuePair("password", password));
-        
-        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-        
-        HttpResponse resp = client.execute(post);
+        HttpResponse resp = client.execute(get);
         
         assertEquals(resp.getStatusLine().getStatusCode(), 200);
     }
+    
+//    @Test
+//    public void testLogin() throws UnsupportedEncodingException, IOException{
+//        HttpClient client = new DefaultHttpClient();
+//        HttpPost post = new HttpPost(URL+"login");
+//        
+//        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+//        nameValuePairs.add(new BasicNameValuePair("userName", userName));
+//        nameValuePairs.add(new BasicNameValuePair("password", password));
+//        
+//        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//        
+//        HttpResponse resp = client.execute(post);
+//        
+//        assertEquals(resp.getStatusLine().getStatusCode(), 200);
+//    }
     
 }
