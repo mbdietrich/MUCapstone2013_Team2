dfd6427e33589c83b28f7967a36a696af5efca00
Sarsha Jones <stameidyx@hotmail.com>
2013-09-18 19:31:41 +1200

    Login and Create tests implemented
    Note the URL has been changed back to the online version.
    I had trouble getting the game to work locally. This works fine at home,
    but the proxy may be an issue at Massey.

--- CapstoneServerTests/test/ServerTests.java
+++ CapstoneServerTests/test/ServerTests.java
@@ -10,11 +10,13 @@ import java.util.ArrayList;
 import java.util.List;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.*;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -28,7 +30,7 @@ import static org.junit.Assert.*;
  */
 public class ServerTests {
     
-    private static final String URL = "http://localhost:8080/CapstoneProject/";
+    private static final String URL = "http://capstoneg2.jelastic.servint.net/CapstoneProject/";
     private static final String userName = "ApacheTest";
     private static final String password = "testPass";
     
@@ -56,24 +58,61 @@ public class ServerTests {
         HttpGet get = new HttpGet(uri);
         
         HttpResponse resp = client.execute(get);
+
+        assertEquals(resp.getStatusLine().getStatusCode(), 200);
+        
+    }
+    
+    @Test
+    public void testLogin() throws UnsupportedEncodingException, IOException{
+        HttpClient client = new DefaultHttpClient();
+        HttpPost post = new HttpPost(URL+"login");
+        
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+        nameValuePairs.add(new BasicNameValuePair("userName", userName));
+        nameValuePairs.add(new BasicNameValuePair("password", password));
+        
+        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+        
+        HttpResponse resp = client.execute(post);
         
         assertEquals(resp.getStatusLine().getStatusCode(), 200);
     }
+  
     
-//    @Test
-//    public void testLogin() throws UnsupportedEncodingException, IOException{
-//        HttpClient client = new DefaultHttpClient();
-//        HttpPost post = new HttpPost(URL+"login");
-//        
-//        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
-//        nameValuePairs.add(new BasicNameValuePair("userName", userName));
-//        nameValuePairs.add(new BasicNameValuePair("password", password));
-//        
-//        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-//        
-//        HttpResponse resp = client.execute(post);
-//        
-//        assertEquals(resp.getStatusLine().getStatusCode(), 200);
-//    }
+    @Test
+    public void testCreate() throws ClientProtocolException, IOException{
+        HttpClient client = new DefaultHttpClient();
+        
+        //Log in
+        HttpPost postlogin = new HttpPost(URL+"login");
+        List<NameValuePair> loginpairs = new ArrayList<NameValuePair>(2);
+        loginpairs.add(new BasicNameValuePair("userName", userName));
+        loginpairs.add(new BasicNameValuePair("password", password));
+        postlogin.setEntity(new UrlEncodedFormEntity(loginpairs));
+        HttpResponse loginresp = client.execute(postlogin);       
+        postlogin.releaseConnection();
+        
+        
+        //Create a game       
+        HttpPost post = new HttpPost(URL+"create");
+ 
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+        nameValuePairs.add(new BasicNameValuePair("type", "any")); 
+
+        post.setEntity(new UrlEncodedFormEntity(nameValuePairs));  
+     
+        HttpResponse resp = client.execute(post);
+        
+        // If it was successful, the response is either "joined" or "created"
+        // #TODO: (better way to write the assert???)
+        String responsebody = EntityUtils.toString(resp.getEntity());
+        boolean flag = false;
+        if (responsebody.equals("created")| responsebody.equals("joined")){
+            flag = true;
+        }
+        
+        assertEquals(flag, true);
+    }
     
 }
