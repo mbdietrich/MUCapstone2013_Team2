d9150855ec03f1c970b78c67b7e975ab29bff5cb
Max <Max@192.168.1.7>
2013-10-09 12:54:11 +1200

    Added safety net to bot execution.

--- CapstoneProject/src/java/capstone/player/Bot.java
+++ CapstoneProject/src/java/capstone/player/Bot.java
@@ -5,28 +5,74 @@
 package capstone.player;
 
 import capstone.game.Coordinates;
+import capstone.game.GameRules;
 import capstone.game.GameSession;
 import capstone.game.GameState;
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author Max
- * The default model for a bot.
+ * @author Max The default model for a bot.
  */
 public abstract class Bot implements Player {
 
+    public static final ExecutorService executor = new ThreadPoolExecutor(1, 4, 1, TimeUnit.MINUTES, new ArrayBlockingQueue<Runnable>(10));
+    
+    private static class Call implements Callable<Coordinates>{
+        
+        private GameSession session;
+        private Bot me;
+        
+        public Call(GameSession current, Bot me){
+            session=current;
+        }
+        
+        @Override
+        public Coordinates call() throws Exception {
+                return me.next(session.getCurrentGame(), session.getPlayerNumber(me));
+        }
+    };
+
     @Override
     public void notify(GameSession current) {
-        current.move(this, next(current.getCurrentGame(), current.getPlayerNumber(this)));
+
+        Call move = new Call(current, this);
+
+        FutureTask<Coordinates> future = new FutureTask<Coordinates>(move);
+                executor.execute(future);
+                
+            Coordinates c = null;
+            try{
+                c = future.get(2, TimeUnit.SECONDS);
+                if(c!=null&&GameRules.validMove(current.getCurrentGame(), c)){
+                    current.move(this,c);
+                }
+                else{
+                    current.Leave(this);                    
+                }
+            } catch (Throwable tr) {
+                future.cancel(true);
+                current.Leave(this);
+            }
     }
-    
+
     public abstract Coordinates next(GameState prev, int player);
-    
+
     @Override
-    public boolean equals(Object o){
-        if(o!=null){
-            if(o instanceof Bot){
-                Bot b = (Bot)o;
+    public boolean equals(Object o) {
+        if (o != null) {
+            if (o instanceof Bot) {
+                Bot b = (Bot) o;
                 return b.getName().equals(this.getName());
             }
         }
