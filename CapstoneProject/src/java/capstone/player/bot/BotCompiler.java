5bdbed92893c1cf9c9e35003147aff8f13948b38
Max <Max@192.168.1.7>
2013-10-11 11:23:17 +1200

    Added player.bot package

--- CapstoneProject/src/java/capstone/player/bot/BotCompiler.java
+++ CapstoneProject/src/java/capstone/player/bot/BotCompiler.java
@@ -2,8 +2,9 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package capstone.player;
+package capstone.player.bot;
 
+import capstone.player.Bot;
 import com.sun.org.apache.xalan.internal.xsltc.compiler.CompilerException;
 import java.net.URI;
 import java.util.Arrays;
